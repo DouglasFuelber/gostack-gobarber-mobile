cdd9dbdb1fc52d1f48f5484a57fcf89d0034ba6e
Douglas Fuelber <contato@douglasfuelber.com>
2020-05-01 16:23:45 -0300

    SignIn page style

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   align-items: center;
@@ -12,4 +13,35 @@ export const Title = styled.Text`
   font-family: 'RobotoSlab-Medium';
   font-size: 24px;
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  align-items: center;
+  background: #312e38;
+  border-color: #232129;
+  border-top-width: 1px;
+  bottom: 0;
+  flex-direction: row;
+  left: 0;
+  justify-content: center;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+  position: absolute;
+  right: 0;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  color: #ff9000;
+  font-size: 18px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
 `;
