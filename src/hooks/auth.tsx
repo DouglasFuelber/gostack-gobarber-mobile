2c80211018b971b86e16d9f5394b6ede38d0dcdd
Douglas Fuelber <contato@douglasfuelber.com>
2020-06-11 22:59:58 -0300

    List providers

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -45,8 +45,11 @@ export const AuthProvider: React.FC = ({ children }) => {
         '@GoBarber:user',
       ]);
 
-      if (token[1] && user[1])
+      if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
+      }
 
       setLoading(false);
     }
@@ -66,6 +69,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { Provider } from './index';
 
 export const Container = styled.View`
   flex: 1;
@@ -32,4 +35,53 @@ export const UserAvatar = styled.Image`
   border-radius: 28px;
   height: 56px;
   width: 56px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px 16px;
+`;
+
+export const ProvidersListTitle = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 24px;
+  margin-bottom: 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)`
+  align-items: center;
+  background: #3e3b47;
+  border-radius: 10px;
+  flex-direction: row;
+  margin-bottom: 16px;
+  padding: 20px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  border-radius: 36px;
+  height: 72px;
+  width: 72px;
+`;
+
+export const ProviderInfo = styled.View`
+  flex: 1;
+  margin-left: 20px;
+`;
+
+export const ProviderName = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+`;
+
+export const ProviderMeta = styled.View`
+  align-items: center;
+  flex-direction: row;
+  margin-top: 8px;
+`;
+
+export const ProviderMetaText = styled.Text`
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 8px;
 `;
