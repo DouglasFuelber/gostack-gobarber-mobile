82cf028e63bcd5033d73a670fbc74bde47a11842
Douglas Fuelber <contato@douglasfuelber.com>
2020-06-11 22:09:14 -0300

    Created header and app apges

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  background: #28262e;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 20px;
+  line-height: 28px;
+`;
+
+export const UserName = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  border-radius: 28px;
+  height: 56px;
+  width: 56px;
+`;
