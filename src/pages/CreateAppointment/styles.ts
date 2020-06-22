00d0b96f17c623baa9d6625bb527ac7b856e20e1
Douglas Fuelber <contato@douglasfuelber.com>
2020-06-21 21:19:16 -0300

    Created appointment

--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -12,6 +12,15 @@ interface ProviderNameProps {
   selected: boolean;
 }
 
+interface HourProps {
+  available: boolean;
+  selected: boolean;
+}
+
+interface HourTextProps {
+  selected: boolean;
+}
+
 export const Container = styled.View`
   flex: 1;
 `;
@@ -40,6 +49,8 @@ export const UserAvatar = styled.Image`
   margin-left: auto;
   width: 56px;
 `;
+
+export const Content = styled.ScrollView``;
 
 export const ProvidersListContainer = styled.View``;
 
@@ -91,4 +102,54 @@ export const OpenDatePickerButtonText = styled.Text`
   color: #232129;
   font-family: 'RobotoSlab-Medium';
   font-size: 16px;
+`;
+
+export const Schedule = styled.View`
+  padding: 24px 0 16px;
+`;
+
+export const Section = styled.View`
+  margin-bottom: 24px;
+`;
+
+export const SectionTitle = styled.Text`
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  margin: 0 24px 12px;
+`;
+
+export const SectionContent = styled.ScrollView.attrs({
+  contentContainerStyle: { paddingHorizontal: 24 },
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})``;
+
+export const Hour = styled(RectButton) <HourProps>`
+  background: ${props => (props.selected ? '#ff9000' : '#3e3b47')};
+  border-radius: 10px;
+  margin-right: 8px;
+  opacity: ${props => (props.available ? 1 : 0.3)};
+  padding: 12px;
+`;
+
+export const HourText = styled.Text<HourTextProps>`
+  color: ${props => (props.selected ? '#232129' : '#f4ede8')};
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
+`;
+
+export const CreateAppointmentButton = styled(RectButton)`
+  align-items: center;
+  background: #ff9000;
+  border-radius: 10px;
+  height: 50px;
+  justify-content: center;
+  margin: 0 24px 24px;
+`;
+
+export const CreateAppointmentButtonText = styled.Text`
+  color: #232129;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
 `;
